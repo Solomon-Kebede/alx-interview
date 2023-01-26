353594459b5fd86a26f186f90e56ad188b345eb3
Solomon Kebede <solomonaau188@gmail.com>
2023-01-26 18:43:54 +0000

    Task 0 - utf8: 4 byte check

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,8 @@ Validate UTF-8
 def validUTF8(data):
     '''Validate UTF-8'''
     for i in data:
+        if (len(bin(i)) - 2) > 32:
+            return False
         if i > 127:
             return False
     return True
