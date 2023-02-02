be6f968cb760a26a415e27ea59a76fcdecb7e472
Solomon Kebede <solomonaau188@gmail.com>
2023-02-02 23:21:51 +0000

    Task 0 - nqueens sourced + edited

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -22,6 +22,39 @@ an N×N chessboard. Write a program that solves the N queens problem.
 from sys import argv
 
 
+def is_attack(i, j):
+    #checking if there is a queen in row or column
+    for k in range(0,N):
+        if board[i][k]==1 or board[k][j]==1:
+            return True
+    #checking diagonals
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+
+
+def N_queen(n):
+    #if n is 0, solution found
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            '''checking if we can place a queen here or not
+            queen will not be placed if the place is being attacked
+            or already occupied'''
+            if (not(is_attack(i,j))) and (board[i][j]!=1):
+                board[i][j] = 1
+                #recursion
+                #wether we can put the next queen with this arrangment or not
+                if N_queen(n-1)==True:
+                    return True
+                board[i][j] = 0
+    return False
+
+
 def nqueens(N):
     '''nqueens'''
     if N < 4:
@@ -31,7 +64,15 @@ def nqueens(N):
         if not isinstance(N, int):
             print('N must be a number')
             exit(1)
-        return []
+        else:
+            N_queen(N)
+            result = []
+            for row_index in range(len(board)):
+                row = board[row_index]
+                for index in range(len(row)):
+                    if row[index] == 1:
+                        result.append([row_index, index])
+            print(result)
 
 
 if __name__ == "__main__":
@@ -39,5 +80,8 @@ if __name__ == "__main__":
         print('Usage: nqueens N')
         exit(1)
     else:
+        #chessboard
+        #NxN matrix with all elements 0
         N = eval(argv[1])
+        board = [[0]*N for _ in range(N)]
         nqueens(N)
