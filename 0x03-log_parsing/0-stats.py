7e67095e185a0324c6897688773a8accc49f1a11
Solomon Kebede <solomonaau188@gmail.com>
2023-03-28 01:09:42 +0000

    Task 0 - disable debugger, log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -45,19 +45,19 @@ try:
     for line in sys.stdin:
         # print(repr(line))
         if count == 10:
-            print(1)
+            # print(1)
             printer()
-            count = 0
-        print(2)
+            count = 1
+        # print(2)
         line_clean = line.splitlines()[0]
         total_size += int(line_clean.split(" ")[-1])
         status = line_clean.split(" ")[-2]
         status_dict[status] += 1
-        print(line_clean)
+        # print(line_clean)
         count += 1
-        print(3)
+        # print(3)
         if count == 2:
-            print(0)
+            # print(0)
             printer()
 
 except KeyboardInterrupt:
